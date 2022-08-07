120a4829e04cf20e3f947d82ce4c6e157d8aec22
IgnacioVivas <ignacab@live.com>
2022-08-07 20:12:47 -0300

    paginator component created

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,23 +1,40 @@
 import CardNew from './CardNew'
 import Classes from '../styles/newsList.module.scss'
 import { useLocation } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import Paginator from './Paginator';
 
 function NewsList({ orderedDataForViews, orderedDataForDate }) {
 
     const currentRoute = useLocation();
+    const [page, setPage] = useState(1);
 
+    useEffect(() => {
+        setPage(1);
+    }, [currentRoute.pathname])
+    
     return (
-        <div className={Classes.newsListContainer}>
+        <div className={Classes.container}>
+            <div className={Classes.newsListContainer}>
             {
                 currentRoute.pathname === '/top-rated' ?
-                    <div className={Classes.wrapperNewsList} id="ratedContainer">
-                        {orderedDataForViews.map((item, index) => <CardNew noticia={item} key={item.id} index={index} />)}
-                    </div>
+                    <>
+                        <Paginator page={page} setPage={setPage} totalPages={orderedDataForViews.length} />
+                        <div className={Classes.wrapperNewsList} id="ratedContainer">
+                            {page && orderedDataForViews[page - 1]?.map((item, index) => <CardNew noticia={item} key={item.id} index={index} />)}
+                        </div>
+                        <Paginator page={page} setPage={setPage} totalPages={orderedDataForViews.length} />
+                    </>
                     :
-                    <div className={Classes.wrapperNewsList} id="timeContainer">
-                        {orderedDataForDate.map((item, index) => <CardNew noticia={item} key={item.id} index={index} />)}
-                    </div>
+                    <>
+                        <Paginator page={page} setPage={setPage} totalPages={orderedDataForDate.length} />
+                        <div className={Classes.wrapperNewsList} id="timeContainer">
+                            {page && orderedDataForDate[page - 1]?.map((item, index) => <CardNew noticia={item} key={item.id} index={index} />)}
+                        </div>
+                        <Paginator page={page} setPage={setPage} totalPages={orderedDataForDate.length} />
+                    </>
             }
+            </div>
         </div>
     )
 }
--- src/components/NewsListContainer.jsx
+++ src/components/NewsListContainer.jsx
@@ -16,6 +16,7 @@ function NewsListContainer() {
             return 0;
         });
 
+
     const orderedDataForDate =
         infoNewscopyForDate.sort(function (x, y) {
             let firstDate = new Date(x.createdAt),
@@ -26,9 +27,23 @@ function NewsListContainer() {
             return 0;
         });
 
+    const pagination = (data) => {
+        const unitsPerPage = 18;
+        const pages = data.length / unitsPerPage;
+        const arr = new Array(Math.ceil(pages));
+        let initialPosition = 0;
+        let endPosition = 18;
+        for (let i = 0; i < arr.length; i++) {
+            arr[i] = data.slice(initialPosition, endPosition);
+            initialPosition = initialPosition + 18;
+            endPosition = endPosition + 18;
+        }
+        return arr;
+    }
+
     return (
         <>
-            <NewsList orderedDataForViews={orderedDataForViews} orderedDataForDate={orderedDataForDate}/>
+            <NewsList orderedDataForViews={pagination(orderedDataForViews)} orderedDataForDate={pagination(orderedDataForDate)} />
         </>
     )
 }
